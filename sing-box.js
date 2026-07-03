dfcfd187ed93abc18473b28fcdad86e87f3eac26
zmnxx <zmn0422@gmail.com>
2026-07-04 02:51:02 +0800

    更新 sing-box.js

--- sing-box.js
+++ sing-box.js
@@ -26,51 +26,6 @@ config.outbounds.map((i) => {
   if (["all-auto"].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies));
   }
-  // hk-auto: 香港节点
-  if (["hk-auto"].includes(i.tag)) {
-    i.outbounds.push(
-      ...getTags(
-        proxies,
-        /^(?!.*(?:网站|网址|获取|订阅|流量|到期|余量|续费|过期|重置|kr|korea|韩国)).*(\b(hk|hong.*kong)\b|港|香港|🇭🇰)/i,
-      ),
-    );
-  }
-  // tw-auto: 台湾节点
-  if (["tw-auto"].includes(i.tag)) {
-    i.outbounds.push(
-      ...getTags(
-        proxies,
-        /^(?!.*(?:网站|网址|获取|订阅|流量|到期|余量|续费|过期|重置|kr|korea|韩国)).*(\b(tw|taiwan)\b|🇹🇼|台|台湾)/i,
-      ),
-    );
-  }
-  // jp-auto: 日本节点
-  if (["jp-auto"].includes(i.tag)) {
-    i.outbounds.push(
-      ...getTags(
-        proxies,
-        /^(?!.*(?:网站|网址|获取|订阅|流量|到期|余量|续费|过期|重置|kr|korea|韩国)).*(\b(jp|japan)\b|日本|日|🇯🇵)/i,
-      ),
-    );
-  }
-  // sg-auto: 新加坡节点
-  if (["sg-auto"].includes(i.tag)) {
-    i.outbounds.push(
-      ...getTags(
-        proxies,
-        /^(?!.*(?:网站|网址|获取|订阅|流量|到期|余量|续费|过期|重置|kr|korea|韩国)).*(\b(sg|singapore)\b|🇸🇬|新加坡|新)/i,
-      ),
-    );
-  }
-  // us-auto: 美国节点
-  if (["us-auto"].includes(i.tag)) {
-    i.outbounds.push(
-      ...getTags(
-        proxies,
-        /^(?!.*(?:网站|网址|获取|订阅|流量|到期|余量|续费|过期|重置|kr|korea|韩国)).*(\b(us|united.*states)\b|美|美国|🇺🇸)/i,
-      ),
-    );
-  }
 });
 
 // 对空的 outbound 自动创建 COMPATIBLE(direct) 防止报错
